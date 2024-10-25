5e8b0b10b1fbf0258978b058a0931cdb7fbc3bdc
Clara Lopez-Uribe <claralopezuribe.developer@gmail.com>
2024-10-25 07:23:24 -0400

    Comment code to plan Amortization schedule section

--- js/site.js
+++ js/site.js
@@ -14,72 +14,8 @@ function getValues() {
     interestRate = parseFloat(interestRate);
 
     calculateTotals(loanAmount, term, interestRate);
-    
-}
-
-function calculateTotals(loanAmount, term,interestRate) {
-
-    let monthlyRate = interestRate/1200;
-    let yourPayment = (loanAmount * monthlyRate)/(1-(1 + monthlyRate)**(-(term)));
-    let totalInterest = loanAmount * monthlyRate * term;
-    let totalCost = loanAmount + totalInterest;
-
-    displayTotals(yourPayment, loanAmount, totalInterest, totalCost);
-}
-
-function displayTotals(yourPayment, loanAmount, totalInterest, totalCost) {
-
-    document.getElementById("yourPayment").innerHTML = ` $${yourPayment.toFixed(2)}`;
-    document.getElementById("totalPrincipal").innerHTML = ` $${loanAmount.toFixed(2)}`;
-    document.getElementById("totalInterest").innerHTML = ` $${totalInterest.toFixed(2)}`;
-    document.getElementById("totalCost").innerHTML = ` $${totalCost.toFixed(2)}`
 
 }
-
-
-
-    
-/*    let remainingBalance = remainingBalance(loanAmount);
-    let interestPayment = interestPayment(remainingBalance);
-    let principalPayment = principalPayment();
-
-    
-} */
-
-/*function principalPayment() {
-
-    yourPayment - interestPayment
-}
-
-function remainingBalance(remainingBalance) {
-
-    remainingBalance - principalPayment
-
-}
-
-function interestPayment(remainingBalance, monthlyRate) {
-
-    remainingBalance * monthlyRate
-} */
-
-
-
-/* Terminology
-
-
-
-
-
-
-let totalCost = document.getElementById("totalCost");
-
-
-
-Month (eg. 1-60)
-Begining Balance
-Payment
-Interest
-Ending Balance
 
 
 /*Formulas Used:
@@ -97,6 +33,82 @@ principalPaid = monthlyPayment - interestPaid
 interestPaid(previousBalance)
 interestToDate()
 */
+function calculateTotals(loanAmount, term,interestRate) {
+
+    let monthlyRate = interestRate/1200;
+    let totalInterest = loanAmount * monthlyRate * term;
+    let totalCost = loanAmount + totalInterest;
+
+    let yourPayment = (loanAmount * monthlyRate)/(1-(1 + monthlyRate)**(-(term)));
+
+    displayTotals(yourPayment, loanAmount, totalInterest, totalCost);
+}
+
+function displayTotals(yourPayment, loanAmount, totalInterest, totalCost) {
+
+    document.getElementById("yourPayment").innerHTML = ` $${yourPayment.toFixed(2)}`;
+    document.getElementById("totalPrincipal").innerHTML = ` $${loanAmount.toFixed(2)}`;
+    document.getElementById("totalInterest").innerHTML = ` $${totalInterest.toFixed(2)}`;
+    document.getElementById("totalCost").innerHTML = ` $${totalCost.toFixed(2)}`
+
+}
+
+
+// create amortization schedule calculations
+
+/* function remainingBalance(loanAmount) {
+
+    
+    iteration 0 remaining balance = loanAmount
+    interation 1 remaining balance = loanAmount - principalPayment
+    iteration 2 remaining balance = previous remaining balance - principalPayment
+    
+    }
+
+*/
+
+    
+/*  let remainingBalance = remainingBalance(loanAmount);
+    let interestPayment = interestPayment(remainingBalance);
+    let principalPayment = principalPayment();
+
+    
+} */
+
+/*function principalPayment() {
+
+    yourPayment - interestPayment
+}
+
+
+
+function interestPayment(remainingBalance, monthlyRate) {
+
+    remainingBalance * monthlyRate
+} */
+
+
+
+/* Terminology
+
+
+
+
+
+/*Formulas Used:
+
+Total Monthly Payment = (amount loaned) * (rate/1200) / (1 – (1 + rate/1200)(rised to the power of -Number of Months) )
+Remaining Balance before the very first month equals the amount of the loan.
+Interest Payment = Previous Remaining Balance * rate/1200
+
+
+
+Month (eg. 1-60)
+Begining Balance
+Payment
+Interest
+Ending Balance
+
 
 
 /* LOGIC:
